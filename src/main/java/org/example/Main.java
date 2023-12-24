9c106880d7e8ed811ac7ab7ccc3fd313266b6142
Yassine Essalmi <user@192.168.1.45>
2023-12-24 10:01:39 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import proxy.BanqueService;
+import proxy.BanqueWS;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        BanqueService proxy = new BanqueWS().getBanqueServicePort();
+        System.out.println(proxy.conversionEuroToDH(9000));
+    }
+}
